156c35b32918295e00aa31725419281da9c1ba5d
egberts <egberts@github.com>
2024-06-07 21:57:34 -0500

    Add more built-in syntax checking

--- default-deny/compile.sh
+++ default-deny/compile.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-table="./nftables.nft"
+table_filespec="./nftables.nft"
 
+# Some `nft`-specific debug options to play with:
 # NFT_CMD="${NFT} --debug scanner, parser, eval, netlink, mnl, proto-ctx, segtree, all"
 #NFT_DBG="-d scanner"
 
 
-NFT="$(which nft)"
+NFT="$( whereis -b nft | awk -e '{print $2;}')"
 retsts=$?
 if [ $retsts -ne 0 ]; then
   echo "Trouble with 'which nft' command: Exit code $retsts; aborted."
@@ -16,15 +17,15 @@ fi
 # show version
 ${NFT} --version -V
 
-##  echo "Checking and prevalidating nftables rulesets (before flushing) ..."
-# retsts=$?
-# ${NFT} -c ${NFT_DBG} -f "${table}"
-# if [ $retsts -ne 0 ]; then
-  # echo "Trouble checking nftables' ruleset': Exit code $retsts; aborted."
-  # exit $retsts
-# fi
-# echo "Actual error code after checking 'nft -f $tables': value=$retsts"
-
+echo "Checking and prevalidating nftables rulesets (before flushing) ..."
+retsts=$?
+${NFT} -c ${NFT_DBG} -f "${table_filespec}"
+if [ $retsts -ne 0 ]; then
+  echo "Trouble checking nftables' ruleset': Exit code $retsts; aborted."
+  exit $retsts
+fi
+echo "Passing after checking 'nft -f $tables_filespec': value=$retsts"
+echo
 echo "Flushing nftables rulesets ..."
 retsts=$?
 ${NFT} flush ruleset
@@ -34,7 +35,7 @@ if [ $retsts -ne 0 ]; then
 fi
 
 
-echo "Reading $table"
+echo "Reading $table_filespec"
 
 # quoted in case filename has a whitespace
-${NFT} ${NFT_DBG} -f "${table}"
+${NFT} ${NFT_DBG} -f "${table_filespec}"
